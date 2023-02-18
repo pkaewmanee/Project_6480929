3090bb5147b06192f552a4282487d38087f97b45
Phakkhapon Kaewmanee <fishermanfill01@gmail.com>
2023-02-18 22:33:13 +0700

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,6 +1,4 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
 Possathorn Sujipisut 6480274
 Phakkhapon Kaewmanee 6480929
  */
